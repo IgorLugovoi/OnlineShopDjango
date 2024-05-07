0ccd2f5d27b38455a7f34af318fa0500490a97ce
IgorLugovoiii <qeadzc4065@gmail.com>
2024-05-07 19:30:01 +0300

    added categories, main and main templates

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import ClothCategory
 # Register your models here.
+
+@admin.register(ClothCategory)
+class ClothCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'is_visible', 'sort')
+    list_editable = ('name','is_visible', 'sort')
+    list_filter = ('is_visible',)
+    search_fields = ('name','slug')
+    prepopulated_fields = {'slug': ('name',)}
--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ClothCategory(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    is_visible = models.BooleanField(default=True)
+    sort = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('sort',)
