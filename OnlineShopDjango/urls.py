5cd081a2867679c80e1b3907cf0a2ccb2f90ddfe
IgorLugovoiii <qeadzc4065@gmail.com>
2024-05-14 14:47:00 +0300

    Fixed problems with photo

--- OnlineShopDjango/urls.py
+++ OnlineShopDjango/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from main.views import home
@@ -27,4 +29,4 @@ urlpatterns = [
     # path('shop/', shopItemHome, name='shopItemHome'),
     path('order/', order_view, name='order'),
     path('itemListHome/', ItemListHome, name='itemListHome'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
