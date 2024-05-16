04b1aba288b9a9eca01eebdc1e66759ed927f5a8
Serhii-Chernov <chernovsrg@gmail.com>
2024-05-16 20:11:31 +0300

    combined order and cart

--- OnlineShopDjango/urls.py
+++ OnlineShopDjango/urls.py
@@ -2,26 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from main.views import home
-from main.views import order_view
+from main.views import home, view_cart_and_order,show_category_items
 from itemList.views import ItemListHome
-from main.views import show_category_items
-from shopItem.views import shop_item_detail
-from shopItem.views import add_to_cart
-from shopItem.views import clear_cart
-from shopItem.views import cartItem
-
+from shopItem.views import shop_item_detail,add_to_cart,clear_cart,cartItem
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    # path('shop/', shopItemHome, name='shopItemHome'),
-    path('order/', order_view, name='order'),
-    path('itemListHome/', ItemListHome, name='itemListHome'),
-    path('category/<int:category_id>/', show_category_items, name='category_items'),
-    # path('shopItem/', shopItemHome, name='shopItemHome'),
     path('shopItem/<slug:cloth_slug>/', shop_item_detail, name='shop_item_detail'),
+    path('itemListHome/', ItemListHome, name='itemListHome'),
+    # path('order/', order_view, name='order'),
+    path('category/<int:category_id>/', show_category_items, name='category_items'),
     path('add-to-cart/<int:cloth_id>/', add_to_cart, name='add_to_cart'),
     path('cart/clear/', clear_cart, name='clear_cart'),
-    path('cart/', cartItem, name='cart_detail'),
+    # path('cart/', cartItem, name='cart_detail'),
+    path('cart/', view_cart_and_order, name='cart_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -21,18 +21,18 @@ def home(request):
         'total_price': total_price,
     })
 
-def order_view(request):
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your order has been successfully submitted!')
-        else:
-            messages.error(request, 'Something went wrong!')
-        return redirect('home')
-    else:
-        form = OrderForm()
-    return render(request, 'order.html', {'form': form})
+# def order_view(request):
+#     if request.method == 'POST':
+#         form = OrderForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Your order has been successfully submitted!')
+#         else:
+#             messages.error(request, 'Something went wrong!')
+#         return redirect('home')
+#     else:
+#         form = OrderForm()
+#     return render(request, 'order.html', {'form': form})
 
 # views.py
 def show_category_items(request, category_id):
@@ -40,6 +40,27 @@ def show_category_items(request, category_id):
     items = Cloth.objects.filter(category=category_id)
     return render(request, 'category_items.html', {'category': category, 'items': items})
 
-def view_cart(request):
-    cart_items = CartItem.objects.all() # Отримуємо всі елементи кошика
-    return render(request, 'cartItem.html', {'cart_items': cart_items})
+# def view_cart(request):
+#     cart_items = CartItem.objects.all() # Отримуємо всі елементи кошика
+#     return render(request, 'cartItem.html', {'cart_items': cart_items})
+
+def view_cart_and_order(request):
+    cart_items = CartItem.objects.all()  # Отримуємо всі елементи кошика
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your order has been successfully submitted!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Something went wrong!')
+    else:
+        form = OrderForm()
+
+    context = {
+        'cart_items': cart_items,
+        'form': form,
+    }
+
+    return render(request, 'cartItem.html', context)
